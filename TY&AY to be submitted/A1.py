7d4f5360bd19f0406133f8571a859017b7d4245a
Tze Yang Ng <tzeyangng@Tzes-MacBook-Pro.local>
2018-10-17 14:39:58 +0800

    A1 done

---
+++ TY&AY to be submitted/A1.py
@@ -0,0 +1,94 @@
+#
+# Project 1, starter code part a
+#
+import math
+import tensorflow as tf
+import numpy as np
+import pylab as plt
+
+# initialization routines for bias and weights
+def init_bias(n = 1):
+    return(tf.Variable(np.zeros(n), dtype=tf.float32))
+
+def init_weights(n_in=1, n_out=1):
+    return (tf.Variable(tf.truncated_normal([n_in, n_out], stddev=1.0/math.sqrt(float(n_in))), name='weights'))
+
+# scale data
+def scale(X, X_min, X_max):
+    return (X - X_min)/(X_max-X_min)
+
+NUM_FEATURES = 36
+NUM_CLASSES = 6
+num_neurons = 10
+
+learning_rate = 0.01
+epochs = 1000
+batch_size = 32
+
+seed = 10
+np.random.seed(seed)
+
+#read train data
+train_input = np.loadtxt('sat_train.txt',delimiter=' ')
+trainX, train_Y = train_input[:,:36], train_input[:,-1].astype(int)
+trainX = scale(trainX, np.min(trainX, axis=0), np.max(trainX, axis=0))
+train_Y[train_Y == 7] = 6 #changes all 7s to 6
+
+trainY = np.zeros((train_Y.shape[0], NUM_CLASSES))
+trainY[np.arange(train_Y.shape[0]), train_Y-1] = 1 #one hot matrix
+
+
+# experiment with small datasets
+trainX = trainX[:1000]
+trainY = trainY[:1000]
+
+n = trainX.shape[0] #n=1000,number of datasets
+
+
+# Create the model
+x = tf.placeholder(tf.float32, [None, NUM_FEATURES])
+y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES])
+
+
+# Build the graph for the deep net
+
+V = init_weights(NUM_FEATURES,num_neurons)
+c = init_bias(num_neurons)
+W = init_weights(num_neurons, NUM_CLASSES)
+b = init_bias(NUM_CLASSES)
+
+h = tf.nn.sigmoid(tf.matmul(x, V) + c)
+logits  = tf.matmul(h, W) + b
+
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=logits)
+beta = tf.constant(0.000001)
+L2_regularization = tf.nn.l2_loss(V) + tf.nn.l2_loss(W)
+loss = tf.reduce_mean(cross_entropy + beta*L2_regularization)
+
+# Create the gradient descent optimizer with the given learning rate.
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+train_op = optimizer.minimize(loss)
+
+correct_prediction = tf.cast(tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1)), tf.float32)
+accuracy = tf.reduce_mean(correct_prediction)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    train_acc = []
+    for i in range(epochs):
+        for starting_idex in range(0, n-batch_size, batch_size):
+            train_op.run(feed_dict={x: trainX[starting_idex:starting_idex+batch_size], y_: trainY[starting_idex:starting_idex+batch_size]})
+        
+        train_acc.append(accuracy.eval(feed_dict={x: trainX, y_: trainY}))
+
+        if i % 100 == 0:
+            print('iter %d: accuracy %g'%(i, train_acc[i]))
+
+
+# plot learning curves
+plt.figure(1)
+plt.plot(range(epochs), train_acc)
+plt.xlabel(str(epochs) + ' iterations')
+plt.ylabel('Train accuracy')
+plt.show()
+
