c6ce927e591143085f377433c2991d2aa72cdc04
AkaHitsuji <yang025@e.ntu.edu.sg>
2018-11-07 15:57:29 +0800

    completed q1 and 2 for part B

--- Assignment 2/Part B/2b2.py
+++ Assignment 2/Part B/2b2.py
@@ -8,12 +8,14 @@ from datetime import datetime
 is_testing = True
 
 MAX_DOCUMENT_LENGTH = 100
-N_FILTERS = 10
-FILTER_SHAPE1 = [20, 256]
-FILTER_SHAPE2 = [20, 1]
+NUM_FILTERS = 10
+FILTER_SHAPE1 = [20,20]
+FILTER_SHAPE2 = [20,1]
 POOLING_WINDOW = 4
 POOLING_STRIDE = 2
+HIDDEN_SIZE = 20
 MAX_LABEL = 15
+EMBEDDING_SIZE = 20
 
 if is_testing:
     no_epochs = 5
@@ -27,15 +29,18 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 seed = 10
 tf.set_random_seed(seed)
 
-def char_cnn_model(x):
+def word_cnn_model(x):
+
+  word_vectors = tf.contrib.layers.embed_sequence(
+      x, vocab_size=n_words, embed_dim=EMBEDDING_SIZE)
 
   input_layer = tf.reshape(
-      tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])
+      word_vectors, [-1, MAX_DOCUMENT_LENGTH, EMBEDDING_SIZE, 1])
 
-  with tf.variable_scope('CNN_Layer1'):
+  with tf.variable_scope('CNN_Layer'):
     conv1 = tf.layers.conv2d(
         input_layer,
-        filters=N_FILTERS,
+        filters=NUM_FILTERS,
         kernel_size=FILTER_SHAPE1,
         padding='VALID',
         activation=tf.nn.relu)
@@ -47,7 +52,7 @@ def char_cnn_model(x):
 
     conv2 = tf.layers.conv2d(
         pool1,
-        filters=N_FILTERS,
+        filters=NUM_FILTERS,
         kernel_size=FILTER_SHAPE2,
         padding='VALID',
         activation=tf.nn.relu)
@@ -63,41 +68,47 @@ def char_cnn_model(x):
 
   return input_layer, logits
 
-
-def read_data_chars():
+def data_read_words():
 
   x_train, y_train, x_test, y_test = [], [], [], []
 
   with open('train_medium.csv', encoding='utf-8') as filex:
     reader = csv.reader(filex)
     for row in reader:
-      x_train.append(row[1])
+      x_train.append(row[2])
       y_train.append(int(row[0]))
 
-  with open('test_medium.csv', encoding='utf-8') as filex:
+  with open("test_medium.csv", encoding='utf-8') as filex:
     reader = csv.reader(filex)
     for row in reader:
-      x_test.append(row[1])
+      x_test.append(row[2])
       y_test.append(int(row[0]))
 
   x_train = pandas.Series(x_train)
   y_train = pandas.Series(y_train)
   x_test = pandas.Series(x_test)
   y_test = pandas.Series(y_test)
-
-
-  char_processor = tf.contrib.learn.preprocessing.ByteProcessor(MAX_DOCUMENT_LENGTH)
-  x_train = np.array(list(char_processor.fit_transform(x_train)))
-  x_test = np.array(list(char_processor.transform(x_test)))
   y_train = y_train.values
   y_test = y_test.values
 
-  return x_train, y_train, x_test, y_test
+  vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
+      MAX_DOCUMENT_LENGTH)
 
+  x_transform_train = vocab_processor.fit_transform(x_train)
+  x_transform_test = vocab_processor.transform(x_test)
+
+  x_train = np.array(list(x_transform_train))
+  x_test = np.array(list(x_transform_test))
+
+  no_words = len(vocab_processor.vocabulary_)
+  print('Total words: %d' % no_words)
+
+  return x_train, y_train, x_test, y_test, no_words
 
 def main():
+  global n_words
 
-  x_train, y_train, x_test, y_test = read_data_chars()
+  x_train, y_train, x_test, y_test, n_words = data_read_words()
 
   if is_testing:
     x_train = x_train[:500]
@@ -112,7 +123,7 @@ def main():
   x = tf.placeholder(tf.int64, [None, MAX_DOCUMENT_LENGTH])
   y_ = tf.placeholder(tf.int64)
 
-  inputs, logits = char_cnn_model(x)
+  inputs, logits = word_cnn_model(x)
 
   # Optimizer
   entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(y_, MAX_LABEL), logits=logits))
@@ -122,8 +133,8 @@ def main():
   correct_prediction = tf.cast(tf.equal(tf.argmax(logits, 1), y_), tf.float32)
   accuracy = tf.reduce_mean(correct_prediction)
 
-  # sess = tf.Session()
-  # sess.run(tf.global_variables_initializer())
+  sess = tf.Session()
+  sess.run(tf.global_variables_initializer())
 
   # training
   loss = []
@@ -157,7 +168,7 @@ def main():
   plt.xlabel('Epochs')
   plt.ylabel('Accuracy/Loss')
 
-  plt.savefig('graphs/2b1-char-cnn-'+str(datetime.now())
+  plt.savefig('graphs/2b2-word-cnn-'+str(datetime.now())
 +'.png')
   plt.close()
 
